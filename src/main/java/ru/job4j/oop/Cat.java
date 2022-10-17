ec6e8011636aa97c8bcc4b2f161f6ce377818e79
herok <heroking2081@gmail.com>
2022-10-17 13:47:02 +0500

    1.2. Вызов метода объекта [#166085 #66548]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -2,9 +2,14 @@ package ru.job4j.oop;
 
 public class Cat {
 
+    public void myau() {
+        System.out.println("Мяу мяу мяу");
+    }
 
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
+        peppy.myau();
+        sparky.myau();
     }
 }
--- src/main/java/ru/job4j/oop/Dog.java
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -2,9 +2,15 @@ package ru.job4j.oop;
 
 public class Dog {
 
+    public void gav() {
+        System.out.println("Gav gav gav");
+    }
     public static void main(String[] args) {
         Dog polkan = new Dog();
         Dog sharik = new Dog();
         Dog zhuchka = new Dog();
+        polkan.gav();
+        sharik.gav();
+        zhuchka.gav();
     }
 }
