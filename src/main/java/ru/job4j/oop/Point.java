dffd2c688da43e29e9ec54609f5bcc4594b4c2bf
herok <heroking2081@gmail.com>
2022-10-17 18:08:23 +0500

    2.1. Рефакторинг - Расстояние между точками. [#122587 #66541]

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -3,23 +3,23 @@ package ru.job4j.oop;
 import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 
-public class Pointer {
+public class Point {
 
     private int x;
     private int y;
 
-    public Pointer(int first, int second) {
+    public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
-    public double distance(Pointer that) {
+    public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        Pointer a = new Pointer(0, 0);
-        Pointer b = new Pointer(0, 2);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
         double dist = a.distance(b);
         System.out.println(dist);
     }
--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -3,12 +3,12 @@ package ru.job4j.oop;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class PointerTest {
+public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        Pointer a = new Pointer(0, 0);
-        Pointer b = new Pointer(2, 0);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
@@ -16,8 +16,8 @@ public class PointerTest {
     @Test
     public void when50to22then2() {
         double expected = 3.6;
-        Pointer a = new Pointer(5, 0);
-        Pointer b = new Pointer(2, 2);
+        Point a = new Point(5, 0);
+        Point b = new Point(2, 2);
         double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
@@ -25,8 +25,8 @@ public class PointerTest {
     @Test
     public void when83to11then2() {
         double expected = 7.28;
-        Pointer a = new Pointer(8, 3);
-        Pointer b = new Pointer(1, 1);
+        Point a = new Point(8, 3);
+        Point b = new Point(1, 1);
         double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
@@ -34,8 +34,8 @@ public class PointerTest {
     @Test
     public void when05to56then2() {
         double expected = 5.1;
-        Pointer a = new Pointer(0, 5);
-        Pointer b = new Pointer(5, 6);
+        Point a = new Point(0, 5);
+        Point b = new Point(5, 6);
         double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
