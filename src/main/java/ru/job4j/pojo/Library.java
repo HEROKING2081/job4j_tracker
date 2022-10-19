6be1c6b2fc5ac61b8603594cafe24cafe0e5bcdd
herok <heroking2081@gmail.com>
2022-10-19 18:46:50 +0500

    3. Массивы и модели. [#174892 #66570]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book bible = new Book("Bible", 1568);
+        Book threeMusketeers = new Book("Three Musketeers", 668);
+        Book robinsonCrusoe = new Book("Robinson Crusoe", 192);
+        Book cleanCode = new Book("Clean code", 25);
+        Book[] books = new Book[4];
+        books[0] = bible;
+        books[1] = threeMusketeers;
+        books[2] = robinsonCrusoe;
+        books[3] = cleanCode;
+        for (int i =0; i < books.length; i++) {
+            Book bk = books[i];
+            System.out.println(bk.getName() + " - "
+                    + bk.getPages() + " pages.");
+        }
+        Book swap = books[0];
+        books[0] = books[3];
+        books[3] = swap;
+        for (int i = 0; i < books.length; i++) {
+            Book bk = books[i];
+            if (bk.getName().equals("Clean code")) {
+                System.out.println("Result = " + bk.getName() + " - "
+                        + bk.getPages() + " pages.");
+            }
+        }
+    }
+}
