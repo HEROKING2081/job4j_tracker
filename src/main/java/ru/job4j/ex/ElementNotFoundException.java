3eb43ec399802162557dd43d3f542b6384f78aff
herok <heroking2081@gmail.com>
2022-10-25 15:40:54 +0500

    0.4. Пользовательские исключения. [#219367 #66523]

---
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+public class ElementNotFoundException extends Exception{
+
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException();
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] words = {"Mouse", "Dog", "Cat", "Pig"};
+            indexOf(words, "Horse");
+            System.out.println(indexOf(words, "efj"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
