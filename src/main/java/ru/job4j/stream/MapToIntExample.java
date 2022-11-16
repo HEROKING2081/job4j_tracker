34a419f9a9827fc7f36b04b0e98e5670ee4f330a
herok <heroking2081@gmail.com>
2022-11-16 16:47:42 +0500

    13. Методы преобразования потоков объектов в потоки примитивы и наоборот [#504927]

---
+++ src/main/java/ru/job4j/stream/MapToIntExample.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapToIntExample {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("1", "2", "3", "4", "5"); // создаем лист строковых значений (объекты)
+        strings.stream() // запускаем поток из строк
+                .mapToInt(Integer::parseInt) // преобразуем поток String в поток int
+                .filter(e -> e % 2 == 0) // отбираем числа, делящиеся на 2
+                .forEach(System.out::println); // выводим полученные числа на печать
+    }
+}
